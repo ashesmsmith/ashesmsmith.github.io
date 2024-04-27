372329593bcee22e7c3ad260d5557378fb4811a6
ashesmsmith <ashesmsmith@gmail.com>
2024-04-26 19:08:53 -0600

    updated value error for JS Modules

--- week1/scripts/script.js
+++ week1/scripts/script.js
@@ -2,11 +2,12 @@ import person, {printAge as printUserAge, printName} from './user.js';
 
 const userName = document.querySelector('#user-name');
 const userAge = document.querySelector('#user-age');
-const user = new person(userName, userAge);
 
 document.querySelector('#user-button').addEventListener('click', printUser);
 
 function printUser() {
+    const user = new person(userName.value, userAge.value);
+
     printName(user);
     printUserAge(user);
 }
