e91e44d1bc7c4a90ccac4da7f7f359d1d389b2a8
ashesmsmith <ashesmsmith@gmail.com>
2024-05-10 19:25:16 -0600

    final project start setup

---
+++ Final/scripts/main.js
@@ -0,0 +1,10 @@
+const baseURL = 'https://www.googleapis.com/books/v1/';
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const isbn = document.querySelector('#isbn');
+const category = document.querySelector('#category');
+const search = document.querySelector('#search');
+
+search.addEventListener('click', () => {
+    // DO SEARCH STUFF
+})
