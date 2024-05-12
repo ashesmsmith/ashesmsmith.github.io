e8140f98ec58d213d9fc06ad7ef2705e49cecc41
ashesmsmith <ashesmsmith@gmail.com>
2024-05-12 16:48:24 -0600

    progress on fetch api data

--- Final/scripts/main.js
+++ Final/scripts/main.js
@@ -1,10 +1,35 @@
-const baseURL = 'https://www.googleapis.com/books/v1/';
-const title = document.querySelector('#title');
-const author = document.querySelector('#author');
-const isbn = document.querySelector('#isbn');
-const category = document.querySelector('#category');
-const search = document.querySelector('#search');
+const baseURL = 'https://www.googleapis.com/books/v1/volumes?q=';
+const searchBar = document.querySelector('#search-param');
+const searchBtn = document.querySelector('#search-btn');
+const results = document.querySelector('#search-results');
 
-search.addEventListener('click', () => {
-    // DO SEARCH STUFF
-})
+searchBtn.addEventListener('click', search);
+
+async function search(event) {
+    event.preventDefault(); // prevent the value from being cleared in search
+    
+    const response = await fetch(`${baseURL}${searchBar.value}`);
+    const data = await response.json();
+
+    displayResults(data.items);
+}
+
+function displayResults(data) {
+    console.log(data);
+
+    data.forEach((book) => {
+        let newCard = resultCardTemplate(book);
+
+        results.append(newCard); // NOT WORKING YET--FIX THIS!
+    })
+}
+
+function resultCardTemplate(book) {
+    return `
+    <section class="book-card">
+        <img class="book-img" src="${book.volumeInfo.imageLinks.thumbnail}" alt="${book.volumeInfo.title} cover image">
+        <h2 class="book-title">${book.volumeInfo.title}</h2>
+        <h3 class="book-author">${book.volumeInfo.authors}</h3>
+        <p class="book-about">${book.volumeInfo.description}</p>
+    </section>`;
+}
