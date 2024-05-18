169a5970e5e38accb039afac1941de0df8bf9422
ashesmsmith <ashesmsmith@gmail.com>
2024-05-17 18:00:18 -0600

    style updates

--- Final/scripts/main.js
+++ Final/scripts/main.js
@@ -1,7 +1,7 @@
 const baseURL = 'https://www.googleapis.com/books/v1/volumes?q=';
 const searchBar = document.querySelector('#search-param');
 const searchBtn = document.querySelector('#search-btn');
-const results = document.querySelector('#search-results');
+const results = document.getElementById('search-results');
 
 searchBtn.addEventListener('click', search);
 
@@ -15,48 +15,28 @@ async function search(event) {
 }
 
 function displayResults(data) {
-    console.log(data);
-
     data.forEach((book) => {
-        let section = document.createElement('section');
-        section.setAttribute('class', 'book-card');
-
-        let img = document.createElement('img');
-        img.setAttribute( 'class', 'book-img');
-        img.setAttribute('src', `${book.volumeInfo.imageLinks.thumbnail}`);
-        img.setAttribute('alt', `${book.volumeInfo.title} cover image`);
-
-        let h2 = document.createElement('h2');
-        h2.setAttribute('class', 'book-title');
-        h2.innerHTML = `${book.volumeInfo.title}`;
-
-        let h3 = document.createElement('h3');
-        h3.setAttribute('class', 'book-author');
-        h3.innerHTML = `${book.volumeInfo.authors}`;
-        
-        let p = document.createElement('p');
-        p.setAttribute('class', 'book-description');
-        p.innerHTML = `${book.volumeInfo.description}`;
-
-
-        section.appendChild(img);
-        section.appendChild(h2);
-        section.appendChild(h3);
-        section.appendChild(p);
-
-        results.appendChild(section);
-
-        /* let newCard = resultCardTemplate(book);
-        results.append(newCard); // NOT WORKING YET--FIX THIS! */
+        console.log(book);
+        results.insertAdjacentHTML('afterbegin', bookDetailsTemplate(book));
     });
 }
 
-/* function resultCardTemplate(book) {
-    return `
-    <section class="book-card">
-        <img class="book-img" src="${book.volumeInfo.imageLinks.thumbnail}" alt="${book.volumeInfo.title} cover image">
+function bookDetailsTemplate(book) {
+    return `<section class="book-card">
+        <a href="${book.volumeInfo.previewLink}">
+            <img 
+                class="cover-img" 
+                src="${book.volumeInfo.imageLinks.thumbnail}" 
+                alt="${book.volumeInfo.title} cover image"
+            >
+        </a>
+        <div class="book-info">
         <h2 class="book-title">${book.volumeInfo.title}</h2>
         <h3 class="book-author">${book.volumeInfo.authors}</h3>
-        <p class="book-about">${book.volumeInfo.description}</p>
-    </section>`;
-} */
+        <h4 class="book-pages">${book.volumeInfo.pageCount} pages</h4>
+        </div>
+        <p class="book-desc">${book.volumeInfo.description}</p>
+        <a class="preview-link" href="${book.volumeInfo.previewLink}"><button>Preview</button></a>
+        <button class="add-btn" type="submit">Add to My Bookshelf</button> 
+    </section>`
+}
